91097f0204132dd4f14dbe3b3a54a80741af81e7
frank6696 <frank6696@126.com>
2020-05-01 16:33:23 +0800

    v1

---
+++ cmake-build-debug/CMakeFiles/mtree.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/Booster.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/Booster.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/Dataset.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/Dataset.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/LinearLoss.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/LinearLoss.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/ModelSelection.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/ModelSelection.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/MultiTaskUpdater.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/MultiTaskUpdater.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/Node.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/Node.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/SingleTaskUpdater.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/SingleTaskUpdater.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/main.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/main.cpp.obj"
+  "D:/DLS/Multi-task Learning/project/tree-model/mtree/tree.cpp" "D:/DLS/Multi-task Learning/project/tree-model/mtree/cmake-build-debug/CMakeFiles/mtree.dir/tree.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mtree.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtree.dir/main.cpp.obj"
+  "CMakeFiles/mtree.dir/Node.cpp.obj"
+  "CMakeFiles/mtree.dir/Dataset.cpp.obj"
+  "CMakeFiles/mtree.dir/ModelSelection.cpp.obj"
+  "CMakeFiles/mtree.dir/SingleTaskUpdater.cpp.obj"
+  "CMakeFiles/mtree.dir/MultiTaskUpdater.cpp.obj"
+  "CMakeFiles/mtree.dir/tree.cpp.obj"
+  "CMakeFiles/mtree.dir/LinearLoss.cpp.obj"
+  "CMakeFiles/mtree.dir/Booster.cpp.obj"
+  "mtree.pdb"
+  "mtree.exe"
+  "mtree.exe.manifest"
+  "libmtree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
